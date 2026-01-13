dc876efecb96ef2965e254327680981b43f8ad58
Deborah9494 <deborah94.iori@gmail.com>
2026-01-13 14:01:10 +0100

    tutorial part 7

--- polls/admin.py
+++ polls/admin.py
@@ -1,7 +1,22 @@
 from django.contrib import admin
+from .models import Choice, Question
 
-# Register your models here.
-from .models import Choice, Question 
+#class ChoiceInline(admin.StackedInline):
+# tabular way of displaying inline related objects
+class ChoiceInline(admin.TabularInline):    
+    model = Choice
+    extra = 3
 
-admin.site.register(Choice)
-admin.site.register(Question)
+class QuestionAdmin(admin.ModelAdmin):
+    fieldsets = [
+        (None, {"fields": ["question_text"]}),
+        ("Date information", {"fields": ["pub_date"], "classes": ["collapse"]}),
+    ]
+    inlines = [ChoiceInline]  # adds the previous class
+    # so each Question admin page will include the related Choices directly 
+    # below the Question form
+
+    # list of field names to display, as columns
+    list_display = ["question_text", "pub_date", "was_published_recently"]
+
+admin.site.register(Question, QuestionAdmin)

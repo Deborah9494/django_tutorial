e329d2d203a36a08bf552959ace6da1ff9f479c5
Deborah9494 <deborah94.iori@gmail.com>
2026-01-13 13:41:53 +0100

    tutorial part 5

--- mysite/urls.py
+++ mysite/urls.py
@@ -15,11 +15,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
-from django.urls import include
-
+from django.urls import include, path
+from django.views.generic import RedirectView
 
 urlpatterns = [
+    path('', RedirectView.as_view(url='/polls/')),  # 👈 redirects root to polls
     path('polls/', include('polls.urls')),  # 👈 include the polls app URLs
     path('admin/', admin.site.urls),
 ]
